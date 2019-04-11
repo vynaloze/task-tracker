9bfda61cf3c009f5b548272903cd509c90b2541f
Piotr Pawluk <vynaloze@gmail.com>
2019-04-11 19:18:49 +0200

    Add swawgger

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using DataAccess.Model;
+﻿using DataAccess.Model;
 using DataAccess.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 
 namespace WebAPI
 {
@@ -29,6 +23,11 @@ namespace WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo{ Title = "Task Tracker API", Version = "v1" });
+            });
             
             var connection = "Data Source=tasktracker.db";
             services.AddDbContext<DataContext>
@@ -51,6 +50,17 @@ namespace WebAPI
 
             app.UseHttpsRedirection();
             app.UseMvc();
+            
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Task Tracker API V1");
+            });
+
         }
     }
 }
